4d17627baa9b654351c3f102edf892899a12abd3
ff5912 <iioka5912@gmail.com>
2015-08-23 01:05:55 +0900

    [refactr] action

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
 'use strict';
 
-var Request = require('request');
-var Cheerio = require('cheerio');
-var ErrorHandler = require('./error_handler.js');
-var Page = require('./model/page.js');
-
+var Crawler = require('./crawler.js');
 var requestUrl = 'http://example.com/';
 
-Request({url: requestUrl}, function(err, res, body) {
-    var msg = ErrorHandler(err);
-    if (!msg) {
-        var $ = Cheerio.load(body);
-        var data = {
-            url: res.request.href,
-            title: $('title').text(),
-            body: $('body').html()
-        };
-        var page = new Page(data);
-        page.save(function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('success');
-            }
-        });
-    } else {
-        console.log(msg);
-    }
-});
+Crawler.execute(requestUrl);
---
+++ crawler.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Request = require('request');
+var Cheerio = require('cheerio');
+var ErrorHandler = require('./error_handler.js');
+var Page = require('./model/page.js');
+
+var saveAction = function(err, res, body) {
+    var msg = ErrorHandler(err);
+    if (!msg) {
+        var $ = Cheerio.load(body);
+        var data = {
+            url: res.request.href,
+            title: $('title').text(),
+            body: $('body').html()
+        };
+        var page = new Page(data);
+        page.save(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('success');
+            }
+        });
+    } else {
+        console.log(msg);
+    }
+};
+
+var Crawler = {
+    execute: function(target) {
+        Request(target, saveAction);
+    }
+}
+
+module.exports = Crawler;
