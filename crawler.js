e9ebfda6ff9e2e48fac9f044b490cfc85c2a7d0a
ff5912 <iioka5912@gmail.com>
2015-08-24 00:04:12 +0900

    [add] クローリングの階層指定

--- crawler.js
+++ crawler.js
@@ -5,7 +5,30 @@ var Cheerio = require('cheerio');
 var ErrorHandler = require('./error_handler.js');
 var Page = require('./model/page.js');
 
-var saveAction = function(err, res, body) {
+function save(data) {
+    var page = new Page(data);
+    page.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('success');
+        }
+    });
+};
+
+function getLinks($, host) {
+    var links = [];
+    $('a').each(function(idx) {
+        var href = $(this).attr('href');
+        var pattern = 'http:\/\/' + host + '.*?';
+        if (href !== undefined && -1 !== href.search(pattern)) {
+            links.push(href);
+        }
+    });
+    return links;
+};
+
+var scraping = function(err, res, body) {
     var msg = ErrorHandler(err);
     if (!msg) {
         var $ = Cheerio.load(body);
@@ -14,25 +37,33 @@ var saveAction = function(err, res, body) {
             title: $('title').text(),
             body: $('body').html()
         };
-        var page = new Page(data);
-        page.save(function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('success');
-            }
-        });
+        save(data);
     } else {
         console.log(msg);
     }
 };
 
+var depth = 0;
+var max_depth = 1;
+var crawl = function(err, res, body) {
+    scraping(err, res, body);
+    depth++;
+    if (depth <= max_depth) {
+        var $ = Cheerio.load(body);
+        var lists = getLinks($, res.request.originalHost);
+        var length = lists.length;
+        for (var i = 0; i < length; i++) {
+            Request(lists[i], crawl);
+        }
+    }
+};
+
 var Crawler = {
-    execute: function(targets) {
-        targets.map(function(target) {
-            Request(target, saveAction);
+    execute: function(seeds) {
+        seeds.map(function(seed) {
+            Request(seed, crawl);
         });
     }
-}
+};
 
 module.exports = Crawler;
